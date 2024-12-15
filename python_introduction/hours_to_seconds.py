7b7b06e0760b33f285dd64d83a37e2977b43eac4
Saleh Hamed <salehhamed773@gmail.com>
2024-12-15 18:34:41 +0200

    Translation from hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours=2
+seconds=hours*3600
+  GNU nano 8.2                  hours_to_seconds.py                   Modified
+hours=2
+seconds=hours*3600
+print(f"{hours} hour(s) is {seconds} seconds.")
