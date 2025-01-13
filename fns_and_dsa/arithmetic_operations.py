22e5c0a76276354181cb24a53c872cf36f8a9f55
Saleh Hamed <salehhamed773@gmail.com>
2025-01-13 19:06:27 +0200

    test1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+result = perform_operation(num1, num2, operation)
+print(f"Result: {result}")
+def perform_operation(num1: float, num2: float, operation: str) -> float:
+     if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation."
