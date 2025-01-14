0101adca590b4c2f5ff71edfc4aa6fd3294b6c60
Saleh Hamed <salehhamed773@gmail.com>
2025-01-14 09:10:32 +0200

    finish2_test1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 # ["num1, num2, operation"]
-def perform_operation(num1: float, num2: float, operation: str) -> float:#(num1,\s*num2,\s*operation\):
+def perform_operation(num1: float, num2: float, operation: str) -> float:#def\s*perform_operation\(num1,\s*num2,\s*operation\):
      if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
